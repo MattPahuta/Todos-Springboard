d248eef83247a86e1b15f758da52f4e848e3e829
Matt Pahuta <mhpahuta@gmail.com>
2020-03-25 12:33:39 -0700

    streamline html/styles, complete localstorage functionality

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 console.log('Todos Script Connected!')
 
 const form = document.querySelector('#add-todo'); 
-const input = document.querySelector('#todo'); 
+const input = document.querySelector('#todo'); // Add todo input
 const todoList = document.querySelector('#todo-list'); 
-// Retrieve from local storage
 const savedTodos = JSON.parse(localStorage.getItem('todos')) || []
 
 // Populate Todo list from savedTodos
@@ -25,17 +24,22 @@ for (let i=0; i<savedTodos.length; i++){
 
 // Add event listener to entire Todo UL
 todoList.addEventListener('click', function(e){
-  console.log(`The event target tag is: ${event.target.tagName}`)
-  // let buttonSibling = e.target.previousSibling;
-  // console.log(`The button was associated with: ${buttonSibling.innerText}`)
-  // ********** Use buttonSibling.innerText === task to remove from local storage
+  // console.log(`The event target tag is: ${event.target.tagName}`)
   let clickedListItem = event.target;
+  // Delete Todo LI from list and local storage
+  if (e.target.tagName === 'BUTTON'){
+    // loop through the localstorage array
+    for (let i=0; i<savedTodos.length; i++){
+      // check if savedTodo task matches the innerText of the previousSibling (<span>) of the button clicked
+      if (savedTodos[i].task === e.target.previousSibling.innerText) {
+        savedTodos.splice(i,1);
+        e.target.parentElement.remove();
+        localStorage.setItem('todos', JSON.stringify(savedTodos));
+      }
+    }
 
-  // if (e.target.tagName === 'BUTTON'){
-  //   e.target.parentElement.remove();
-
-  // } 
-  
+  } 
+  // Toggle the completed css class
   if (e.target.tagName === 'SPAN'){
     e.target.classList.toggle('completed')
     if (e.target.isCompleted === true){
@@ -44,7 +48,7 @@ todoList.addEventListener('click', function(e){
       e.target.isCompleted = true;
     }
   }
-
+  // Toggle the isCompleted flag
   for (let i = 0; i < savedTodos.length; i++) {
     if (savedTodos[i].task === clickedListItem.innerText) {
       savedTodos[i].isCompleted = clickedListItem.isCompleted;
@@ -55,35 +59,23 @@ todoList.addEventListener('click', function(e){
 })
 
 
-
+// Add New Todos
 form.addEventListener('submit', function(e){
   e.preventDefault();
-  console.log(input.value)
-  const newTodo = document.createElement('li'); // new Todo
-  // Use span for Todo
-  const newTodoSpan = document.createElement('span'); // Span
+  const newTodo = document.createElement('li'); // new Todo li
+  const newTodoSpan = document.createElement('span'); // new Todo span
   newTodoSpan.innerText = `${input.value} `;
-  const todoId = Date.now().toString();
-
-  const removeBtn = document.createElement('button');
-
   newTodoSpan.isCompleted = false; // for local storage
+  const removeBtn = document.createElement('button'); // new remove Todo button
   removeBtn.innerText = 'Remove Todo';
   removeBtn.classList.add('remove-btn')
   input.value = '';
   todoList.appendChild(newTodo);
   newTodo.appendChild(newTodoSpan)
   newTodo.appendChild(removeBtn)
-
-
   // Save to local storage
-  savedTodos.push({ task: newTodoSpan.innerText, isCompleted: false, id: todoId});
+  savedTodos.push({ task: newTodoSpan.innerText, isCompleted: false});
   localStorage.setItem('todos', JSON.stringify(savedTodos)); 
 
 })
-
-
-
-
-
 
